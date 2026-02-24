f3dfa382be19b371bdbe7d876d923290a07f6931
JHogenboom <Joshi.Hogenboom@maastrichtuniversity.nl>
2026-02-24 12:00:17 +0100

    initial commit simple iteration function

--- src/fed_synthetic_data/federated_training.py
+++ src/fed_synthetic_data/federated_training.py
@@ -5,9 +5,38 @@ This module provides classes and functions for coordinating federated training
 of synthetic data generators across multiple nodes.
 """
 
+import pandas as pd
 
-def train_single_iteration() -> None:
-    pass
+from typing import Optional
+from mostlyai.engine import TabularARGN
+
+EPOCHS_PER_ITERATION = 1
+
+
+def train_single_iteration(
+    data: pd.DataFrame,
+    model: Optional[TabularARGN] = None,
+) -> TabularARGN:
+    """
+    Perform a single iteration of training for the given model using the provided data.
+
+    Args:
+        data (pd.DataFrame): The training data.
+        model (TabularARGN): The model to train.
+
+    Returns:
+        TabularARGN: The updated model after training.
+    """
+    if model is None:
+        model = TabularARGN(max_epochs=EPOCHS_PER_ITERATION)
+    else:
+        if model.max_epochs is None or model.max_epochs != EPOCHS_PER_ITERATION:
+            # TODO Warn the user that the model's max_epochs is not set to the expected value
+            pass
+
+    model.fit(data)
+
+    return model
 
 
 def extract_model_weights() -> None:
